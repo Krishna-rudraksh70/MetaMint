0eeb1109c2188d73b6daebe0e7230235e592df53
Krishna Rudraksh <krishna.kug070@gmail.com>
2024-03-19 11:32:59 +0530

    add volume ETH

--- js/script.js
+++ js/script.js
@@ -68,6 +68,24 @@ window.onload = () => {
 
   fadeSVG();
 
+  // Animation for Add to Cart button
+  $('#add-to-cart-btn').on('click', function(){
+    var button = $(this);
+    var cart = $('#cart');
+    var cartTotal = cart.attr('data-totalitems');
+    var newCartTotal = parseInt(cartTotal) + 1;
+
+    button.addClass('sendtocart');
+    setTimeout(function(){
+      button.removeClass('sendtocart');
+      cart.addClass('shake').attr('data-totalitems', newCartTotal);
+      setTimeout(function(){
+        cart.removeClass('shake');
+      }, 500);
+    }, 1000);
+  });
+
+
 
   var swiper = new Swiper(".books-slider", {
     loop: true,
